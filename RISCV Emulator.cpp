8bb3dd8cd250aaf64505d1a1c0899e177ecea56f
Joash09 <31129983+Joash09@users.noreply.github.com>
2021-11-26 16:25:15 +0200

    Begin Fetch Instruction

--- RISCV Emulator.cpp
+++ RISCV Emulator.cpp
@@ -1,20 +1,70 @@
 // RISCV Emulator.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include "stdio.h";
+
+typedef unsigned char U8;
+typedef unsigned long int U32; // 64 bits wide unsigned
+
+// const unsigned int MEMORY_SIZE = 1024 * 1024 * 128; // 16 MB
+const unsigned int NUM_INSTRUCTIONS = 1;
+const unsigned int MEMORY_SIZE = NUM_INSTRUCTIONS * 4;
+
+struct Cpu {
+    U32 registers[32];  // 32 registers 32 bits wide
+    U32 program_counter;
+    U8 dram[MEMORY_SIZE]; // Addressing memory as bytes
+};
+
+void initialize(Cpu* cpu);
+void initialize_dram(Cpu* cpu); // For testing
+int fetch(Cpu* cpu);
+void execute(Cpu* cpu, int instruction);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    printf("Welcome to my RISCV emulator\n");
+
+    // Initialize CPU
+    Cpu cpu;
+
+    // FETCH, DECODE, EXECUTE cycle
+    while(cpu.program_counter < MEMORY_SIZE){
+
+        // Jump to next instruction. 32 bits = 4 bytes
+        cpu.program_counter = cpu.program_counter + 4;
+
+        // FETCH
+
+    }
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void initialize(Cpu* cpu) {
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    cpu->registers[0] = 0;
+    cpu->registers[2] = MEMORY_SIZE;
+
+}
+
+void initialize_dram(Cpu* cpu) {
+
+}
+
+int fetch(Cpu* cpu) {
+
+    // Read the 4 bytes which make up the instruction
+    // Bytes are read in Little Endian format
+    U32 index = cpu->program_counter;
+    return cpu->dram[index] |
+            cpu->dram[index + 1] << 8 |
+            cpu->dram[index + 2] << 16 |
+            cpu->dram[index + 3] << 24;
+
+}
+
+void execute(Cpu* cpu, int instruction) {
+
+
+}
